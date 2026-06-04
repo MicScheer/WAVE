2d1078cd7daad955e05e0af2061ffa95caddcba0
Michael Scheer <Michael.Scheer@Helmholtz-Berlin.de>
2026-06-04 11:48:57 +0200

    4.6.2026

--- python/waveplot.py
+++ python/waveplot.py
@@ -42256,9 +42256,10 @@ def _nPlot():
 
 
   global FillColor
-  print("_Plot!")
-  breakpoint()
-  print(WavesMode)
+  #print("_Plot!")
+  #reakpoint()
+  #print(WavesMode)
+
   if not len(Nhead):
     nError("  No Ntuple defined so far!  ")
     return
